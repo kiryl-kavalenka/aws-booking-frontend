fd8ba301d0ede5005927f25320e2118c0cc4c7b7
Ivan <ivanchernetskey@mail.ru>
2023-08-11 12:53:37 +0300

    feat: added templates for 2 steps

---
+++ src/components/pages/terms/terms.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import { Text } from '../../ui/text';
+
+
+export const Terms: FC = () => {
+    return(
+        <>
+            <Text type="title">Terms</Text>
+        </>
+    )
+}
