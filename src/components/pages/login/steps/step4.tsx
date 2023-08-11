153bfac163ccb46eaaf95fea2dbec85f3f8bfd64
Ivan <ivanchernetskey@mail.ru>
2023-08-11 14:30:58 +0300

    feat: added 3, 4, 5 steps

--- src/components/pages/login/steps/step4.tsx
+++ src/components/pages/login/steps/step4.tsx
@@ -1,7 +1,35 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { Text } from '../../../ui/text';
+import { Button } from "../../../ui/button";
 
 export const Step4: FC = () => {
+    const [nickName, setNickName] = useState('')
+    const [canContinue, setCanContinue] = useState(false);
+
+    const handleNickNameChange = (event: any) => {
+        setNickName(event.target.value);
+    }
+
+    const handleContinue = () => {
+        console.log('continue')
+    }
+
+    useEffect(() => {
+        nickName.length >= 5 ? setCanContinue(true) : setCanContinue(false);
+    }, [nickName])
+
     return(
-        <>Step4</>
+        <>
+            <Text type="header" textAlign="center" width="fit-content">My nickname is...</Text>
+            <Text
+                type="info" 
+                textAlign="center" 
+                width="fit-content"
+                >
+                    This is how you will appear in Vamost
+            </Text>
+            <input type="text" placeholder="Nickname" value={nickName} onChange={handleNickNameChange}></input>
+            <Button type="common" width="100%" onClick={handleContinue} disabled={!canContinue}>Contitnue</Button>
+        </>
     )
 }
--- src/components/pages/login/steps/step5.tsx
+++ src/components/pages/login/steps/step5.tsx
@@ -1,7 +1,35 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { Text } from '../../../ui/text';
+import { Button } from "../../../ui/button";
 
 export const Step5: FC = () => {
+    const [birthDay, setBirthDay] = useState('')
+    const [canContinue, setCanContinue] = useState(false);
+
+    const handleBirthDayChange = (event: any) => {
+        setBirthDay(event.target.value);
+    }
+
+    const handleContinue = () => {
+        console.log('continue')
+    }
+
+    useEffect(() => {
+        birthDay.length >= 5 ? setCanContinue(true) : setCanContinue(false);
+    }, [birthDay])
+
     return(
-        <>Step5</>
+        <>
+            <Text type="header" textAlign="center" width="fit-content">My birthday is...</Text>
+            <Text
+                type="info" 
+                textAlign="center" 
+                width="fit-content"
+                >
+                    Your age will be public.
+            </Text>
+            <input type="date" placeholder="date" value={birthDay} onChange={handleBirthDayChange}></input>
+            <Button type="common" width="100%" onClick={handleContinue} disabled={!canContinue}>Contitnue</Button>
+        </>
     )
 }
