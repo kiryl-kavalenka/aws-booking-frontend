5a122ec7dfc05847d3021ab031a48d6013e795c5
Ivan <ivanchernetskey@mail.ru>
2023-08-18 10:39:12 +0300

    feat: implemented react-phone-number-input and added phone input

--- src/components/pages/firstTest/test.tsx
+++ src/components/pages/firstTest/test.tsx
@@ -1,178 +1,46 @@
-import { FC, useEffect, useState } from "react"
-import styled from "styled-components";
+import { FC, useState } from "react"
+import 'react-phone-number-input/style.css'
+import styled from "styled-components"
+import PhoneInput from 'react-phone-number-input'
 
-const LetterWrapper = styled.div`
-    color: #a1a1a1;
-    font-size: 1em;
+const Wrapper = styled.div`
+    width: 300px;
+    height: fit-content;
+    padding: 30px;
+
+    input{
+        border: 0;
+        padding-bottom: 5px;
+        border-bottom: 2px solid #1c1d1f;
+    }
+
+    input:focus{
+        outline: none;
+        padding-bottom: 5px;
+        border-bottom: 2px solid #1c1d1f;
+    }
 `
 
-const ItemWrapper = styled.div<{filled?: boolean, noBorderBottom?: boolean}>`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 50px;
-    height: 50px;
-    ${({noBorderBottom, filled}) => !noBorderBottom ? `border-bottom: ${filled ? '2px solid #3c3737' : '2px solid #a1a1a1' };` : ''}
-`
-
-const InputContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 15px;
-`
 
 export const Test: FC = () => {
-    const AMOUNT_OF_DIGITS = 8;
-    const BACKGROUND_LETTERS = ['M', 'M', '/', 'D', 'D', '/', 'Y', 'Y', 'Y', 'Y']
-    const [keysPressed, setKeysPressed] = useState<string[]>([])
-    const [validError, setValidError] = useState('');
+    // `value` will be the parsed phone number in E.164 format.
+    // Example: "+12133734253".
+    const [value, setValue] = useState()
 
-    const handleKeyPres = (event: any) => {
-        if (keysPressed.length < AMOUNT_OF_DIGITS) {
-            setKeysPressed(prev => prev = [...prev, event.key])
-        }
+    const handler = (e: any) => {
+        setValue(e);
     }
-
-    const validMM = (month: number) => {
-        return month > 0 && month <= 12 ? true : false;
-    }
-
-    const validDD = (day: number, month: number) => {
-        const isDayValid = day > 0 && day <= 31 ? true : false;
-        if (isDayValid) {
-            switch(month) {
-                case 1:
-                    return day > 0 && day <= 31 ? true : false;
-                case 2:
-                    return day > 0 && day <= 29 ? true : false;
-                case 3:
-                    return day > 0 && day <= 31 ? true : false;
-                case 4:
-                    return day > 0 && day <= 30 ? true : false;
-                case 5:
-                    return day > 0 && day <= 31 ? true : false;
-                case 6:
-                    return day > 0 && day <= 30 ? true : false;
-                case 7:
-                    return day > 0 && day <= 31 ? true : false;
-                case 8:
-                    return day > 0 && day <= 31 ? true : false;
-                case 9:
-                    return day > 0 && day <= 30 ? true : false;
-                case 10:
-                    return day > 0 && day <= 31 ? true : false;
-                case 11:
-                    return day > 0 && day <= 30 ? true : false;
-                case 12:
-                    return day > 0 && day <= 31 ? true : false;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    const validYYYY = (year: number) => {
-        const currentYear = new Date().getFullYear();
-        const minYear = currentYear - 120;
-        const maxYear = currentYear - 1;
-
-        if (year < minYear || year > maxYear) {
-            return false;
-        }
-
-        return true;
-    }
-
-    const handleKeyDown = (event: any) => {
-        if (event.key === 'Backspace') {
-            setKeysPressed(prev => prev = prev.slice(0, -1))
-        }
-    }
-
-    useEffect(() => {
-        window.addEventListener('keypress', handleKeyPres)
-        window.addEventListener('keydown', handleKeyDown)
-
-        return () => {
-            window.removeEventListener('keypress', handleKeyPres)
-            window.removeEventListener('keydown', handleKeyDown)
-        }
-    })
-
-    useEffect(() => {
-        if (keysPressed.length === 2) {
-            if (!validMM(Number(keysPressed.join('')))) {
-                setValidError(prev => prev = 'month is not valid')
-            }
-        }
-
-        if (keysPressed.length === 4) {
-            if (!validDD(Number([keysPressed[2], keysPressed[3]].join('')), Number([keysPressed[0], keysPressed[1]].join('')))) {
-                setValidError(prev => prev = 'day is not valid')
-            }
-        }
-        
-        if (keysPressed.length === 8) {
-            if (!validYYYY(Number([keysPressed[4], keysPressed[5], keysPressed[6], keysPressed[7]].join('')))) {
-                setValidError(prev => prev = 'year is not valid')
-            }
-        }
-
-    }, [keysPressed])
-
-    useEffect(() => {
-        if(validError.length !== 0) {
-            console.log('We have a problem: ', validError)
-        }
-    }, [validError])
-
-    const MM = Array.from({ length: 2 }, (_, index) => (
-        <ItemWrapper key={index} filled={!!keysPressed[index]}>
-            {
-                keysPressed[index] ? keysPressed[index] : <LetterWrapper>{BACKGROUND_LETTERS[index]}</LetterWrapper>
-            }
-        </ItemWrapper>
-    ));
-
-    const DD = Array.from({ length: 2 }, (_, index) => (
-        <ItemWrapper key={index} filled={!!keysPressed[index + 2]}>
-            {
-                keysPressed[index + 2] ? keysPressed[index + 2] : <LetterWrapper>{BACKGROUND_LETTERS[index + 3]}</LetterWrapper>
-            }
-        </ItemWrapper>
-    ));
-
-    const YYYY = Array.from({ length: 4 }, (_, index) => (
-        <ItemWrapper key={index} filled={!!keysPressed[index + 4]}>
-            {
-                keysPressed[index + 4] ? keysPressed[index + 4] : <LetterWrapper>{BACKGROUND_LETTERS[index + 6]}</LetterWrapper>
-            }
-        </ItemWrapper>
-    ));
-    
     
     return (
         <>
             <div>TEST PAGE</div>
-
-            <InputContainer>
-                {
-                  MM  
-                }
-                <ItemWrapper noBorderBottom={true}>
-                    <LetterWrapper>{BACKGROUND_LETTERS[2]}</LetterWrapper>
-                </ItemWrapper>
-                {
-                  DD  
-                }
-                <ItemWrapper noBorderBottom={true}>
-                    <LetterWrapper>{BACKGROUND_LETTERS[2]}</LetterWrapper>
-                </ItemWrapper>
-                {
-                  YYYY  
-                }
-            </InputContainer>
+            <Wrapper>
+                <PhoneInput
+                    placeholder="Enter phone number"
+                    value={value}
+                    onChange={handler}/>
+            </Wrapper>
+            {value}
         </>
     )
 }
--- src/components/pages/login/steps/step1.tsx
+++ src/components/pages/login/steps/step1.tsx
@@ -1,42 +1,91 @@
-import { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Text } from '../../../ui/text';
 import { Button } from "../../../ui/button";
 import { Input } from "../../../ui/input";
+import PhoneInput from 'react-phone-number-input';
+import {isValidPhoneNumber} from 'react-phone-number-input';
+import styled from "styled-components";
+
+const FormWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+`
+
+const PhoneInputWrapper = styled.div<{error: boolean}>`
+    &:focus {
+        outline: none;
+        border-bottom: ${({error}) => error ? '2px solid #e13131' : '2px solid #1c1d1f'};
+    }
+    input {
+        border: 0;
+        padding-bottom: 5px;
+        border-bottom: ${({error}) => error ? '2px solid #e13131' : '2px solid #d8d8da'};
+        transition: border-bottom 0.3s linear;
+    }
+
+    input:focus {
+        outline: none;
+        padding-bottom: 5px;
+        border-bottom: ${({error}) => error ? '2px solid #e13131' : '2px solid #1c1d1f'};
+    }
+`
 
 export const Step1: FC = () => {
-    const [phoneNumber, setPhoneNumber] = useState('')
-    const [password, setPassword] = useState('')
+    const [password, setPassword] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState<string | undefined>();
+    const [canSignIn, setCanSignIn] = useState(false);
+    const [phoneNumberError, setPhoneNumberError] = useState<boolean>(false);
+
+    const handlePhoneNumberChange = (value: string | undefined) => {
+        setPhoneNumber(value);
+
+        try {
+            const isValidPhoneNumberRes = isValidPhoneNumber(String(value));
+
+            setPhoneNumberError(isValidPhoneNumberRes ? false : true);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     useEffect(() => {
-        console.log('password', password)
-    }, [password])
+        if (phoneNumberError || password.length < 8) {
+            setCanSignIn(false);
+        } else {
+            setCanSignIn(true);
+        }
+    }, [password, phoneNumberError])
 
-    const handlePhoneNumberChange = (event: any) => {
-        setPhoneNumber(event.target.value);
-    };
-
-    const handlePasswordChange = (event: any) => {
-        setPassword(prev => prev = event.target.value);
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
     };
 
     const handleSignIn = () => {
-        console.log('sign in event')
-        console.log(`setPhoneNumber and password = ${phoneNumber}  ${password}`)
+        console.log('sign in event');
+        console.log(`phoneNumber and password = ${phoneNumber}  ${password}`);
     }
 
-    return(
+    return (
         <>
-            <Text type="header" textAlign="center" width="fit-content">Welcom! Let's create your account</Text>
-            <input type="text" placeholder="Enter your phone number" value={phoneNumber} onChange={handlePhoneNumberChange}></input>
-            <Input 
-                type="password"
-                name="password"
-                placeholder="Enter your password"
-                width="fit-content"
-                onChange={handlePasswordChange} />
+            <Text type="header" textAlign="center" width="fit-content">Welcome! Let's create your account</Text>
+            <FormWrapper>
+                <PhoneInputWrapper error={phoneNumberError}>
+                    <PhoneInput
+                        placeholder="Enter phone number"
+                        value={phoneNumber}
+                        onChange={handlePhoneNumberChange} />
+                </PhoneInputWrapper>
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    width="fit-content"
+                    onChange={handlePasswordChange} />
+            </FormWrapper>
             <Text type="param" textAlign="center" width="fit-content">We will send you a text with a verification code. Message and data rates may apply.</Text>
             <Text type="param" textAlign="center" width="fit-content">By signing up you accept the <a href="/terms">Terms of service</a> and <a href="/terms">Privacy Policy.</a></Text>
-            <Button type="common" width="100%" onClick={handleSignIn}>Sign up</Button>
+            <Button type="common" width="100%" onClick={handleSignIn} disabled={!canSignIn}>Sign up</Button>
         </>
     )
 }
